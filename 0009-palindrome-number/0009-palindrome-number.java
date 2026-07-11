2df40db55c785e10d83623744bed2496089150e2
priyanshu pandit <131549418+odenthegod@users.noreply.github.com>
2026-07-12 00:55:07 +0530

    Time: 5 ms (80.51%), Space: 46 MB (33.94%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        if(x<0){
+            return false;
+        }else{
+            int n=0;
+            while(x>0){
+                int r=x%10;
+                n=n*10+r;
+                x/=10;
+            }
+            if(n==original) return true;
+            else{
+                return false;
+            }
+        }
+    }
+}
