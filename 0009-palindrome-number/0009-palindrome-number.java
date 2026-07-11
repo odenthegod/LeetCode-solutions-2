1985a21572674307bb87e0bb879ddc831afcf360
priyanshu pandit <131549418+odenthegod@users.noreply.github.com>
2026-07-12 01:12:37 +0530

    Time: 5 ms (80.51%), Space: 45.8 MB (87.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int original=x;
-        if(x<0){
-            return false;
-        }else{
+        if(x<0) return false;
+        else{
             long n=0;
             while(x>0){
-                int r=x%10;
-                n=n*10+r;
+                n=n*10+x%10;
                 x/=10;
             }
-            if(n==original) return true;
-            else{
-                return false;
-            }
+            return n==original;   
         }
     }
 }
