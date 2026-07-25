5f59b266e33a2f51a57394852b4cab44c58df675
priyanshu pandit <131549418+odenthegod@users.noreply.github.com>
2026-07-25 15:48:49 +0530

    Time: 1 ms (99.87%), Space: 42.6 MB (44.16%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    int reverese(int n){
+        int sum=0;
+        while(n>0){
+            sum=sum*10+n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    public int mirrorDistance(int n) {
+        int rev=reverese(n);
+        return Math.abs(n-rev);
+    }
+}
