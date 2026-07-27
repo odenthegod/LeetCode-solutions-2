0f8106ed05564ba23d99c6d4bc318abb5dacd605
priyanshu pandit <131549418+odenthegod@users.noreply.github.com>
2026-07-27 15:54:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (15.97%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int add(int week){
+        return (7*(2*week+6))/2;
+    }
+    int totalMoney(int n) {
+        int week=n/7;
+        int days=n%7;
+        int sum=0,cnt=1;
+        for (int i=1;i<=week;i++){
+            sum+=add(i);
+            cnt++;
+        }
+        for (int i=0;i<days;i++){
+            sum+=cnt;
+            cnt++;
+        }
+        return sum;
+    }
+};
