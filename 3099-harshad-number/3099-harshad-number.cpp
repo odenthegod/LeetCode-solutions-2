ddcb65d9fd45b991b9a53b191340f9ec43a09895
priyanshu pandit <131549418+odenthegod@users.noreply.github.com>
2026-08-01 11:14:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (50.62%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int n=0;
+        int num=x;
+        while(x>0){
+            n=n+x%10;
+            x/=10;
+        }
+        if(num%n==0) return n;
+        else {
+            return -1;
+        };
+    }
+};
